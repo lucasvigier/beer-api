ba78bc28fb7457b63e3dfa586cd38e704372ac85
Pinacolada14 <75010761+Pinacolada14@users.noreply.github.com>
2021-03-25 14:49:11 +0100

    routes

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/Beer', beerRouter);
-app.use('/api/Breweries', breweriesRouter);
+app.use('/api/beer', beerRouter);
+app.use('/api/breweries', breweriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/beer.js
+++ routes/beer.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const beerRouter = require('./routes/beer');
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
