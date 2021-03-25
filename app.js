f389f40f6f0eabbe9abc34cc10593b29cd09e4e5
Pinacolada14 <75010761+Pinacolada14@users.noreply.github.com>
2021-03-25 15:24:24 +0100

    routes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const beerRouter = require('./routes/beer');
+const breweriesRouter = require('./routes/breweries');
 
 var app = express();
 
