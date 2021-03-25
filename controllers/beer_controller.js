402f6ff13c5a99c0759d201e92439849a68b09e5
lucas-vigier <lucas.vigier@etu.univ-nantes.fr>
2021-03-25 17:44:50 +0100

    beer controller methods

--- controllers/beer_controller.js
+++ controllers/beer_controller.js
@@ -1,19 +1,36 @@
 const Beer = require("../models").Beer;
 
+// GET all beers
 exports.getAll = (req, res) => {
     Beer.findAll()
         .then(beer => res.status(200).json(beer))
         .catch(err => res.status(500).json(err))
 }
 
+// GET beers by a specific country
 exports.getByCountry = (req, res) => {
     Beer.findAll({where: {country: req.query.country}})
         .then(beer => res.status(200).json(beer))
         .catch(err => res.status(500).json(err))
 }
 
+// GET beers by the beer name
 exports.getByName = (req, res) => {
     Beer.findAll({where: {name: req.query.name}})
+        .then(beer => res.status(200).json(beer))
+        .catch(err => res.status(500).json(err))
+}
+
+// GET beers by the beer id
+exports.getById = (req, res) => {
+    Beer.findAll({where: {id: req.query.id}})
+        .then(beer => res.status(200).json(beer))
+        .catch(err => res.status(500).json(err))
+}
+
+// GET beers by the brewer name
+exports.getByBrewerName = (req, res) => {
+    Beer.findAll({where: {brewer: req.query.brewer}})
         .then(beer => res.status(200).json(beer))
         .catch(err => res.status(500).json(err))
 }
--- routes/beer.js
+++ routes/beer.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const beerController = require('../controllers/beer_controller');
 
-/* GET beers listing. */
+/* GET beers methods. */
 router.get('/', beerController.getAll);
 router.get('/country', beerController.getByCountry);
 router.get('/name', beerController.getByName);
+router.get('/id', beerController.getById);
+router.get('/brewer', beerController.getByBrewerName);
 
 module.exports = router;
