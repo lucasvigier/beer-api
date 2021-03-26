8913a6b4a71baf7a6fdd31787e7f6ca53405c64a
lucas-vigier <lucas.vigier@etu.univ-nantes.fr>
2021-03-26 16:17:39 +0100

    bash test + package.json

--- groupe4_equipe2_rest_test.sh
+++ groupe4_equipe2_rest_test.sh
@@ -3,8 +3,16 @@ BEER_API_URL='http://localhost:3000/api/beer/'
 # [GET] tests for the beer API requests
 echo "[TEST] GET (getAll beer)"
 if [ "$(curl -X GET "$BEER_API_URL/" -o /dev/null -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
-echo "[TEST] GET (getName beer)"
+echo "[TEST] GET (searchByName beer)"
+if [ "$(curl -X GET "$BEER_API_URL/260" -o /dev/null -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
+echo "[TEST] GET (searchById beer)"
 if [ "$(curl -X GET "$BEER_API_URL/name?name=TEST" -o /dev/null -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
+echo "[TEST] GET (searchByBrewer beer)"
+if [ "$(curl -X GET "$BEER_API_URL/brewer?brewer=Brewer%20TEST" -o /dev/null -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
+echo "[TEST] GET (searchUnderAlcoholContent beer)"
+if [ "$(curl -X GET "$BEER_API_URL/alcohol?alcohol=5.00" -o /dev/null -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
+echo "[TEST] GET (searchOverAlcoholContent beer)"
+if [ "$(curl -X GET "$BEER_API_URL/alcohol?alcohol=5.00" -o /dev/null -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
 
 echo
 # [POST] tests for the beer API requests
@@ -16,3 +24,8 @@ echo "[TEST] POST (setBrewer beer)"
 if [ "$(curl -X POST "$BEER_API_URL/setBrewer/260" -H "Content-Type: application/json" -d "{\"brewer\":\"Brewer TEST\"}" -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
 echo "[TEST] POST (setCountry beer)"
 if [ "$(curl -X POST "$BEER_API_URL/setCountry/260" -H "Content-Type: application/json" -d "{\"country\":\"Country TEST\"}" -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
+
+echo
+# [PUT] tests for the beer API requests
+echo "[TEST] POST (addBeer beer)"
+if [ "$(curl -X PUT "$BEER_API_URL/addBeer" -H "Content-Type: application/json" -d "{\"name\":\"TEST\",\"alcohol\":\"6.50\",\"brewer\":\"Brewer TEST\", \"country\":\"Country TEST\"}" -w '%{http_code}\n' -s)" == "200" ]; then echo "[SUCCESS] http_code=200"; fi
