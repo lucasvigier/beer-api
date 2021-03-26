9df35e6088c77f395a6aa2f16e5b675553a331b8
lucas-vigier <lucas.vigier@etu.univ-nantes.fr>
2021-03-26 16:00:35 +0100

    bash test

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/beer', beerRouter);
 app.use('/api/breweries', breweriesRouter);
 
-app.use('/api/swagger/', swaggerUi.serve);
-app.get('/api/swagger/', swaggerUi.setup(swaggerDocument));
+app.use('/', swaggerUi.serve);
+app.get('/', swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
